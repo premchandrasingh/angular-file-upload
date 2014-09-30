8260e36de170b3c07313515ed298a693c09ad05b
premchandrasingh <premchandrasingh@gmail.com>
2014-09-30 17:35:18 +0530

    Updating use of default extensions filter

--- examples/simple/controllers.js
+++ examples/simple/controllers.js
@@ -9,7 +9,8 @@ angular
 
     .controller('AppController', ['$scope', 'FileUploader', function($scope, FileUploader) {
         var uploader = $scope.uploader = new FileUploader({
-            url: 'upload.php'
+            url: 'upload.php',
+            extensions:'jpg,jpeg,png,gif'
         });
 
         // FILTERS
